56fffbf271fefe110e129e75e0a00d8363887f7c
juancoquet <juancoquet@gmail.com>
2024-12-01 22:02:53 +0000

    read inputs and print solution day01 part1

--- advent_of_code/day_01/solution.py
+++ advent_of_code/day_01/solution.py
@@ -1,2 +1,17 @@
+from rich import print
+
+
 def part_1(locations_a: list[int], locations_b: list[int]) -> int:
     return sum(abs(a - b) for a, b in zip(sorted(locations_a), sorted(locations_b)))
+
+
+def read_inputs() -> tuple[list[int], list[int]]:
+    with open("advent_of_code/day_01/inputs.txt", "r") as f:
+        data = [[int(i) for i in line.split()] for line in f.readlines()]
+    locations_a, locations_b = map(list, zip(*data))
+    return locations_a, locations_b
+
+
+if __name__ == "__main__":
+    locations_a, locations_b = read_inputs()
+    print(part_1(locations_a, locations_b))
