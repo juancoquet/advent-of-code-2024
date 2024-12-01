25183783889902d08973e2cb892233889b5e10d6
juancoquet <juancoquet@gmail.com>
2024-12-01 22:11:40 +0000

    solve day01 part2

--- advent_of_code/day_01/solution.py
+++ advent_of_code/day_01/solution.py
@@ -5,6 +5,13 @@ def part_1(locations_a: list[int], locations_b: list[int]) -> int:
     return sum(abs(a - b) for a, b in zip(sorted(locations_a), sorted(locations_b)))
 
 
+def part_2(locations_a: list[int], locations_b: list[int]) -> int:
+    b_count: dict[int, int] = {}
+    for b in locations_b:
+        b_count[b] = b_count.get(b, 0) + 1
+    return sum(a * b_count.get(a, 0) for a in locations_a)
+
+
 def read_inputs() -> tuple[list[int], list[int]]:
     with open("advent_of_code/day_01/inputs.txt", "r") as f:
         data = [[int(i) for i in line.split()] for line in f.readlines()]
@@ -15,3 +22,4 @@ def read_inputs() -> tuple[list[int], list[int]]:
 if __name__ == "__main__":
     locations_a, locations_b = read_inputs()
     print(part_1(locations_a, locations_b))
+    print(part_2(locations_a, locations_b))
--- tests/test_day_01.py
+++ tests/test_day_01.py
@@ -1,8 +1,14 @@
-from advent_of_code.day_01.solution import part_1
+from advent_of_code.day_01.solution import part_1, part_2
 
 
-def test_part_01():
+def test_part_1():
     locations_a = [3, 4, 2, 1, 3, 3]
     locations_b = [4, 3, 5, 3, 9, 3]
     res = part_1(locations_a, locations_b)
     assert res == 11
+
+def test_part_2():
+    locations_a = [3, 4, 2, 1, 3, 3]
+    locations_b = [4, 3, 5, 3, 9, 3]
+    res = part_2(locations_a, locations_b)
+    assert res == 31
