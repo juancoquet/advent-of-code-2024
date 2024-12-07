c431e37663b978006bca54d4313a969294a06d46
juan <juan@Juans-MacBook-Pro.local>
2024-12-07 23:58:07 +0000

    day02 part1

--- advent_of_code/day_02/solution.py
+++ advent_of_code/day_02/solution.py
@@ -1,6 +1,46 @@
-def part_1(reports: list[list[int]]) -> int:
-    return -1
+from rich import print
 
 
-def parse_data(raw_data: str) -> list[list[int]]:
-    return [[int(c) for c in line.split()] for line in raw_data.split("\n")]
+class Report(list[int]):
+    def __init__(self, vector: list[int]):
+        if not vector:
+            raise ValueError("Report cannot be empty")
+        super().__init__(vector)
+
+    def is_safe(self) -> bool:
+        ascending = False
+        for i in range(1, len(self)):
+            left, right = self[i - 1], self[i]
+            if i == 1:
+                ascending = is_ascending(left, right)
+            if not is_safe_jump(left, right) or not ascending == is_ascending(left, right):
+                return False
+        return True
+
+
+def part_1(reports: list[Report]) -> int:
+    return sum(report.is_safe() for report in reports)
+
+
+def is_ascending(a: int, b: int) -> bool:
+    return a < b
+
+
+def is_safe_jump(a: int, b: int) -> bool:
+    return 0 < abs(a - b) <= 3
+
+
+def read_inputs() -> list[Report]:
+    with open("advent_of_code/day_02/inputs.txt", "r") as f:
+        raw_data = f.read().strip()
+    return parse_data(raw_data)
+
+
+def parse_data(raw_data: str) -> list[Report]:
+    return [Report([int(c) for c in line.split()]) for line in raw_data.split("\n")]
+
+
+if __name__ == "__main__":
+    inputs = read_inputs()
+    print(inputs)
+    print(part_1(inputs))
--- tests/test_day_02.py
+++ tests/test_day_02.py
@@ -1,19 +1,51 @@
-from advent_of_code.day_02.solution import parse_data, part_1
+import pytest
+
+from advent_of_code.day_02.solution import Report, parse_data, part_1
 
 
 def test_part_1():
     reports = [
-        [7, 6, 4, 2, 1],
-        [1, 2, 7, 8, 9],
-        [9, 7, 6, 2, 1],
-        [1, 3, 2, 4, 5],
-        [8, 6, 4, 4, 1],
-        [1, 3, 6, 7, 9],
+        Report([7, 6, 4, 2, 1]),
+        Report([1, 2, 7, 8, 9]),
+        Report([9, 7, 6, 2, 1]),
+        Report([1, 3, 2, 4, 5]),
+        Report([8, 6, 4, 4, 1]),
+        Report([1, 3, 6, 7, 9]),
     ]
     exp = 2
 
     res = part_1(reports)
     assert res == exp
+
+
+def test_report_cannot_be_empty():
+    with pytest.raises(ValueError):
+        Report([])
+
+
+def test_is_safe_report_all_safe_ascending_is_true():
+    report = Report([1, 2, 4, 7, 10])
+    assert report.is_safe()
+
+
+def test_is_safe_report_all_safe_descending_is_true():
+    report = Report([10, 7, 4, 2, 1])
+    assert report.is_safe()
+
+
+def test_is_safe_report_no_ascent_or_descent_is_false():
+    report = Report([1, 1, 1])
+    assert not report.is_safe()
+
+
+def test_is_safe_report_safe_ascent_then_safe_descent_is_false():
+    report = Report([1, 2, 3, 2, 1])
+    assert not report.is_safe()
+
+
+def test_is_safe_report_safe_descent_then_safe_ascent_is_false():
+    report = Report([3, 2, 1, 2, 3])
+    assert not report.is_safe()
 
 
 def test_parse_data():
@@ -24,12 +56,12 @@ def test_parse_data():
 8 6 4 4 1
 1 3 6 7 9"""
     exp = [
-        [7, 6, 4, 2, 1],
-        [1, 2, 7, 8, 9],
-        [9, 7, 6, 2, 1],
-        [1, 3, 2, 4, 5],
-        [8, 6, 4, 4, 1],
-        [1, 3, 6, 7, 9],
+        Report([7, 6, 4, 2, 1]),
+        Report([1, 2, 7, 8, 9]),
+        Report([9, 7, 6, 2, 1]),
+        Report([1, 3, 2, 4, 5]),
+        Report([8, 6, 4, 4, 1]),
+        Report([1, 3, 6, 7, 9]),
     ]
 
     res = parse_data(raw_data)
