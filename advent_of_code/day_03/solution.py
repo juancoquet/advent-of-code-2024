18284d6f96e030719c7bdb3fd29937ffc4bf7d3c
juan <juan@Juans-MacBook-Pro.local>
2024-12-08 12:04:11 +0000

    solve d03p1

--- advent_of_code/day_03/solution.py
+++ advent_of_code/day_03/solution.py
@@ -1,21 +1,20 @@
+import re
 from rich import print
 
-def part_1(inputs):
+
+def part_1(inputs: str) -> int:
+    regex = re.compile(r"mul\((?P<a>\d+),(?P<b>\d+)\)")
+    mul_values = regex.findall(inputs)
+    return sum(int(a) * int(b) for a, b in mul_values)
+
+
+def part_2(inputs: str):
     pass
 
 
-def part_2(inputs):
-    pass
-
-
-def parse_data(raw_data: str):
-    pass
-
-
-def _read_inputs():
+def _read_inputs() -> str:
     with open("advent_of_code/day_03/inputs.txt", "r") as f:
-        raw_data = f.read().strip()
-    return parse_data(raw_data)
+        return f.read().strip()
 
 
 if __name__ == "__main__":
--- tests/test_day_03.py
+++ tests/test_day_03.py
@@ -2,7 +2,10 @@ from advent_of_code.day_03.solution import part_1, part_2
 
 
 def test_part_1():
-    pass
+    instructions = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    exp = 161
+    res = part_1(instructions)
+    assert res == exp
 
 def test_part_2():
     pass
