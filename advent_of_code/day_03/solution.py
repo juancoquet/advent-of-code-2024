c6eac0d704fd48fa206918d25cc26fa852da6180
juan <juan@Juans-MacBook-Pro.local>
2024-12-08 11:51:40 +0000

    template day 03

---
+++ advent_of_code/day_03/solution.py
@@ -0,0 +1,24 @@
+from rich import print
+
+def part_1(inputs):
+    pass
+
+
+def part_2(inputs):
+    pass
+
+
+def parse_data(raw_data: str):
+    pass
+
+
+def _read_inputs():
+    with open("advent_of_code/day_03/inputs.txt", "r") as f:
+        raw_data = f.read().strip()
+    return parse_data(raw_data)
+
+
+if __name__ == "__main__":
+    inputs = _read_inputs()
+    print(part_1(inputs))
+    print(part_2(inputs))
---
+++ tests/test_day_03.py
@@ -0,0 +1,8 @@
+from advent_of_code.day_03.solution import part_1, part_2
+
+
+def test_part_1():
+    pass
+
+def test_part_2():
+    pass
